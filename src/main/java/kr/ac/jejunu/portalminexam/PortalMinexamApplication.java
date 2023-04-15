e4fe022ff2fd73cff2ddb3819ae255f1fbdf7613
1jinju <gkswlswn2044@gmail.com>
2023-04-15 21:12:55 +0900

    first commit

---
+++ src/main/java/kr/ac/jejunu/portalminexam/PortalMinexamApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.portalminexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalMinexamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalMinexamApplication.class, args);
+    }
+
+}
