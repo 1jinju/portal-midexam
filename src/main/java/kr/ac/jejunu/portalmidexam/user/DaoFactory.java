6fc5b88be95547128da1f8b6b3b0350a4dbe0d58
1jinju <gkswlswn2044@gmail.com>
2023-04-21 16:22:14 +0900

    5번

--- src/main/java/kr/ac/jejunu/portalmidexam/user/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/portalmidexam/user/DaoFactory.java
@@ -1,11 +1,17 @@
 package kr.ac.jejunu.portalmidexam.user;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
-    public UserDao getUserDao() {
+    @Bean
+    public UserDao userDao() {
         UserDao userDao = new UserDao(connectionMaker());
         return userDao;
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new JejuConnectionMaker();
     }
--- src/test/java/kr/ac/jejunu/portalmidexam/user/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/portalmidexam/user/UserDaoTests.java
@@ -1,6 +1,9 @@
 package kr.ac.jejunu.portalmidexam.user;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -9,14 +12,18 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
+    private static UserDao userDao;
+
+    @BeforeAll
+    public static void setup(){
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        userDao = applicationContext.getBean("userDao", UserDao.class);
+    }
     @Test
     public void get() throws SQLException, ClassNotFoundException {
         Long id = 1l;
         String name = "hulk";
         String password = "1234";
-
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
 
         User user = userDao.findById(id);
         assertThat(user.getId(), is(id));
@@ -32,8 +39,6 @@ public class UserDaoTests {
         user.setName(name);
         user.setPassword(password);
 
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
         userDao.insert(user);
 
         User insertedUser = userDao.findById(user.getId());
